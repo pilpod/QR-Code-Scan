027d7f90333cf46908965bcec702ae5ac2ece2d9
Factoria F5 <factoriaf5@Factorias-MacBook-Pro.local>
2022-02-26 16:41:56 +0100

    init repo

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const scannerSection = document.getElementById("scanner_section");
+
+function app() {
+    checkCompatibility();
+}
+
+app();
+
+function checkCompatibility() {
+    const alertMessage = document.getElementById("alert_message");
+
+    if (!("BarcodeDetector" in window)) {
+        console.log("Barcode Detector is not supported by this browser.");
+        alertMessage.innerHTML =
+            "Barcode Detector is not supported by this browser.";
+        alertMessage.hidden = false;
+    } else {
+        console.log("Barcode Detector supported!");
+
+        alertMessage.innerHTML = "Barcode Detector supported!";
+        alertMessage.hidden = false;
+
+        createDetector();
+    }
+}
+
+function createDetector() {
+    // create new detector
+    var barcodeDetector = new BarcodeDetector({
+        formats: ["qr_code", "code_39", "codabar", "ean_13"],
+    });
+}
